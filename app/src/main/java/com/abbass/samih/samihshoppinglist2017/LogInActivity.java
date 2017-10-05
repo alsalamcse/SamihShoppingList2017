b3ac86047b56f89ea66b9d87f464a2e88f174590
samih1079 <sabbas79>
2017-10-05 14:00:33 +0300

    aa

--- app/src/main/java/com/abbass/samih/samihshoppinglist2017/LogInActivity.java
+++ app/src/main/java/com/abbass/samih/samihshoppinglist2017/LogInActivity.java
@@ -1,13 +1,30 @@
 package com.abbass.samih.samihshoppinglist2017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LogInActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity
+{
+    private Button btnSignIn,btnSignUp;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        btnSignIn= (Button) findViewById(R.id.btnIn);
+        btnSignIn= (Button) findViewById(R.id.btnIn);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getBaseContext(),MainListActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
